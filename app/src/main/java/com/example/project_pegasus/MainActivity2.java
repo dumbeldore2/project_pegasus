a2b5df0b028ef17c48abe9ae50a15db801db6ec3
dumbeldore2 <yago.engels@gmail.com>
2024-01-27 22:10:22 +0100

    1.1.7

--- app/src/main/java/com/example/project_pegasus/MainActivity2.java
+++ app/src/main/java/com/example/project_pegasus/MainActivity2.java
@@ -2,13 +2,36 @@ package com.example.project_pegasus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    // intit view
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //views conecten
+        imageView = findViewById(R.id.imageViewLogo);
+
+        //functies
+        secretAddFun();
+    }
+
+    public void secretAddFun(){
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(MainActivity2.this,MainActivity3.class);
+                startActivity(intent);
+                return false;
+            }
+        });
     }
 }
